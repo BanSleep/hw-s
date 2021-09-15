9fbf3f4a593da8c2b352028f49897fcd7ef1b772
BanSleep <84075066+BanSleep@users.noreply.github.com>
2021-09-15 20:26:34 +0700

    Update dart_hw_7.dart

--- dart_hw_7.dart
+++ dart_hw_7.dart
@@ -3,45 +3,33 @@ import 'dart:core';
 
 void main(List<String> arguments) {
   var cuboid = Cuboid(1, 2, 3);
-  int x = cuboid.cuboidVolume;
+  var x = cuboid.cuboidVolume;
   num areaSurface = cuboid.surfaceArea;
   print('Cuboid volume = $x');
   print('Cuboid Area Surface = $areaSurface');
-  var cube = Cube(1, 2, 3);
+  var cube = Cube(2);
   areaSurface = cube.surfaceArea;
   x = cube.cuboidVolume;
   print('Cube Volume = $x');
   print('Cube Surface Area = $areaSurface');
 }
 class Cuboid  {
-  var lenght;
-  var weight;
-  var height;
-  // late int surface, volume;
-  Cuboid(int w, int l, int h) {
-    lenght = l;
-    height = h;
-    weight = w;
-  }
+  final int lenght;
+  final int weight;
+  final int height;
+  Cuboid(this.weight, this.lenght, this.height);
   int get cuboidVolume {
-    int volume = lenght * weight * height;
+    final volume = lenght * weight * height;
     return volume;
 
   }
   
-  num get surfaceArea {
-    num surface = 2*(lenght*weight + lenght * height + weight * height);
+  int get surfaceArea {
+    final surface = 2*(lenght*weight + lenght * height + weight * height);
     return surface;
   }
 
 }
 class Cube extends Cuboid {
-  @override
-  var lenght, height, weight;
-  Cube(int weight, int lenght, int height) : super(weight, lenght, height) { 
-    this.lenght = lenght;
-    this.height = lenght;
-    this.weight = lenght;
-  }
-
+  Cube(int lenght) : super(lenght, lenght, lenght);
 }
