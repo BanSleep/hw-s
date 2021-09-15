36cf86829e8981a6770778246c0fee1710ad4a64
BanSleep <84075066+BanSleep@users.noreply.github.com>
2021-09-15 20:24:41 +0700

    Update dart_hw_3.dart

--- dart_hw_3.dart
+++ dart_hw_3.dart
@@ -1,16 +1,16 @@
 
 
 void main(List<String> arguments) {
-  var list = <int>[1705, 1900, 1601, 2000];
-  double age;
-  for (var i = 0; i < list.length; i++) {
+  final list = <int>[1705, 1900, 1601, 2000];
+  int age;
+  for (int year in list) {
     age = 0;
-    if (list[i]%100 == 0) { 
-      age = list[i]/100;
+    if (year%100 == 0) { 
+      age = year~/100;
     }
     else {
-      age = list[i]/100 + 1;
+      age = year~/100 + 1;
     }
-    print(age.toInt());
+    print(age);
   }
 }
