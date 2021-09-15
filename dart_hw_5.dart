51bc60b46a78bfdb606190b2febcff0cad05ae78
BanSleep <84075066+BanSleep@users.noreply.github.com>
2021-09-15 20:26:07 +0700

    Update dart_hw_5.dart

--- dart_hw_5.dart
+++ dart_hw_5.dart
@@ -1,32 +1,16 @@
 void main(List<String> arguments) {
-  var list = [
-    ["d", "a", "r", "t"],
-    ["a", "b", "c"],
-    ["g", "o", "o", "d", "l", "u", "c", "k"]
-  ];
-  completeVoid(list);
+  var list = ["dart", "abc", "good luck"];
+  for (int i = 0; i < list.length; i++) {
+    int x = calculate(list[i]) * (i + 1);
+    print(x);
+  }
+  
 }
 
-void completeVoid (List<List<String>> list) {
-  List<int> helpList = [];
-  int x = 4, l = 0;
-  for (var i = 0; i < 3; i++) {
-    helpList.add(0);
+int calculate (String word) {
+  int x = 0;
+  for (int i in word.replaceAll(" ", "").codeUnits) {
+    x += i - 'a'.codeUnitAt(0) + 1;
   }
-  for (var i = 0; i < list.length; i++) {
-    if (i == 1) {
-      x = 3;
-    }
-    if (i == 2) x = 8;
-    for (var j = 0; j < x; j++) {
-      helpList[l] += (list[i][j].codeUnitAt(0) - 96);
-    }
-    
-    l++;
-    helpList[l - 1] *= l;
-    
-  }
-  for (var i = 0; i < 3; i++) {
-    print(helpList[i]);
-  }
+  return x;
 }
