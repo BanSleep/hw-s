ef600e80c9ea621d3359efaeac919a7ce9e1ef44
BanSleep <84075066+BanSleep@users.noreply.github.com>
2021-09-15 20:25:34 +0700

    Update dart_hw_4.dart

--- dart_hw_4.dart
+++ dart_hw_4.dart
@@ -1,20 +1,7 @@
 void main(List<String> arguments) {
   var list = [60, 999, 14, "dart1", 45, 95, "dart", 1];
-  bool flag = false;
   dynamic value = "dart";
-  for (int i = 0; i < list.length; i++) {
-    if (list[i] == value) {
-      flag = true;
-      print(flag);
-    }
-    if (flag == true) {
-      i = 0;
-      value = 15;
-      flag = false;
-    }
-  }
-  if (flag == false) {
-    print(flag);
-  }
-
+  print(list.contains(value));
+  value = 15;
+  print(list.contains(value));
 }
