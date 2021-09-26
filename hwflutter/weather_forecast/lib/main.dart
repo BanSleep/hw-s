c7bf9be2ac1cdf3551bfcbccdc6224dda78a1278
BanSleep <84075066+BanSleep@users.noreply.github.com>
2021-09-26 12:45:58 +0700

    Add files via upload

---
+++ hwflutter/weather_forecast/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:weather_prac/screens/location_screen.dart';
+
+import 'screens/weather_forecast_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: LocationScreen(),
+    );
+  }
+}
+
