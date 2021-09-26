3a621c51e0bc729c5179dbdc2f1d6be192bda6a2
BanSleep <84075066+BanSleep@users.noreply.github.com>
2021-09-26 12:45:04 +0700

    Add files via upload

---
+++ hwflutter/weather_forecast/lib/utilities/forecast_util.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Util {
+  static String getFormattedDate(DateTime dateTime) {
+    return DateFormat('EEE, MMM, d, y').format(dateTime);
+  }
+
+  static getItem(IconData iconData, int value, String units) {
+    return Column(
+      children: [
+        Icon(
+          iconData,
+          color: Colors.black,
+          size: 28,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          '$value',
+          style: TextStyle(fontSize: 20, color: Colors.black),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          '$units',
+          style: TextStyle(fontSize: 15, color: Colors.black),
+        )
+      ],
+    );
+  }
+}
---
+++ hwflutter/weather_forecast/lib/utilities/location.dart
@@ -0,0 +1,18 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double? latitude;
+  double? longitute;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+              desiredAccuracy: LocationAccuracy.best)
+          .timeout(Duration(seconds: 5));
+      latitude = position.latitude;
+      longitute = position.longitude;
+    } catch (e) {
+      throw 'Something goes wrong: $e';
+    }
+  }
+}
